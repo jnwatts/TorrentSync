583f1002e8215fc0a49bc86ad8cea1364a89c1b7
Josh Watts <josh+github@sroz.net>
2017-10-29 17:55:40 -0400

    deluge: Add auth support

--- backend/deluge.py
+++ backend/deluge.py
@@ -2,11 +2,13 @@ import requests
 import json
 
 class Deluge:
-    def __init__(self, url, password):
+    def __init__(self, url, password, http_auth=None):
         self.url = url
         self.password = password
         self.headers = {'content-type': 'application/json', 'Accept': 'application/json'}
         self.cookies = None
+        if http_auth:
+            self.http_auth = (http_auth['user'], http_auth['pass'])
 
     def invoke(self, method, params=[]):
         is_auth = (method == "auth.login")
@@ -16,8 +18,17 @@ class Deluge:
             "jsonrpc": "2.0",
             "id": 1,
         }
-        response = requests.post(self.url, data=json.dumps(payload), headers=self.headers, cookies=self.cookies)
-        data = response.json()
+        response = requests.post(self.url, data=json.dumps(payload), headers=self.headers, cookies=self.cookies, auth=self.http_auth)
+
+        if response.status_code != 200:
+            raise Exception("Error invoking method {} with params {}: {} {}".format( method, params, response.status_code, response.text))
+
+        try:
+            data = response.json()
+        except json.decoder.JSONDecodeError as e:
+            print(response)
+            raise(e)
+
         if data["error"]:
             if data["error"]["code"] == 1 and not is_auth:
                 self.auth()
